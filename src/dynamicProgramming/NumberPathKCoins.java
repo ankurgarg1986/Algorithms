228cfaf18a1451ea10beb97757abddfa5e1c8db9
Ankur Garg <ankurgarg9@gmail.com>
2016-06-26 11:03:02 +0530

    Added code for Number ofPaths with exact K coins

---
+++ src/dynamicProgramming/NumberPathKCoins.java
@@ -0,0 +1,37 @@
+package dynamicProgramming;
+
+public class NumberPathKCoins {
+
+	public static void main(String[] args) {
+		int k = 12;
+	    int[][] mat = { {1, 2, 3},
+	                      {4, 6, 5},
+	                      {4, 2, 1}
+	                  };
+	    System.out.println(pathCountRecursive(mat, k));
+	 //   return 0;
+
+	}
+
+	private static int pathCountRecursive(int[][] mat, int k) {
+		int m = mat.length;
+		int n = mat[0].length;
+		int ans = pathCountRecursive(mat,m-1,n-1,m-1,n-1,k);
+		return ans;
+	}
+
+	private static int pathCountRecursive(int[][] mat ,int i, int j,int m,int n, int sum) {
+		int res = 0;
+		if(i<0 || j< 0 || i>m || j>n) return 0;
+		sum = sum -mat[i][j];
+		if(sum < 0) return 0;
+		if(sum ==0)
+		{
+			if(i==0 && j==0) return 1;
+			return 0;
+		}
+		res = pathCountRecursive(mat,i-1,j,m,n,sum) + pathCountRecursive(mat,i,j-1,m,n,sum);
+		return res;
+	}
+
+}
