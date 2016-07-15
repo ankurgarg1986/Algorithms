4fde3d620266ec1a6bff0ced5746abff514426ea
Ankur Garg <ankurgarg9@gmail.com>
2016-07-15 17:14:17 +0530

    Added LCS and LongestConsecutivePath

---
+++ src/dynamicProgramming/LCS.java
@@ -0,0 +1,56 @@
+package dynamicProgramming;
+
+public class LCS {
+
+	public static void main(String[] args) {
+
+		String x = "AGGTAB";
+		String y = "GXTXAYB";
+		int m = x.length();
+		int n = y.length();
+		System.out.println(lcsRecursive(x, y, 0,0,m, n));
+		System.out.println(lcsDynamic(x, y,m, n));
+
+	}
+
+	private static int lcsDynamic(String x, String y, int m,int n) {
+		int[][] dp = new int[m+1][n+1];//dp[i][j] gives lcsPath from i to j
+		int i,j;
+		dp[0][0] = 0;
+		for(i=0;i<m;i++)
+		{
+			for(j=0;j<n;j++)
+				dp[i][j] = 0;
+		}
+		for(i=1;i<=m;i++)
+		{
+			for(j=1;j<=n;j++)
+			{
+				if(x.charAt(i-1) == y.charAt(j-1))
+				{
+					dp[i][j] = 1 + dp[i-1][j-1];
+				}
+				else
+				{
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
+		return dp[m][n];
+	}
+
+	private static int lcsRecursive(String x, String y,int i,int j, int m, int n) {
+		int ans  = 0;
+		if(i >= m || j>= n ) return 0;
+		if(x.charAt(i) == y.charAt(j))
+		{
+			ans = 1 + lcsRecursive(x,y,i+1,j+1,m,n);
+		}
+		else
+		{
+			ans = Math.max(lcsRecursive(x,y,i+1,j,m,n) , lcsRecursive(x,y,i,j+1,m,n));
+		}
+		return ans;
+	}
+
+}
---
+++ src/dynamicProgramming/LongestConsecutivePath.java
@@ -0,0 +1,104 @@
+package dynamicProgramming;
+
+public class LongestConsecutivePath {
+
+	public static void main(String[] args) {
+		char[][] mat = { { 'a', 'c', 'd' }, { 'h', 'b', 'e' },
+				{ 'i', 'g', 'f' } };
+		System.out.println(getLen(mat, 'a'));
+		System.out.println(getLenDynamic(mat, 'a'));
+		System.out.println(getLen(mat, 'e'));
+		System.out.println(getLenDynamic(mat, 'e'));
+		System.out.println(getLen(mat, 'b'));
+		System.out.println(getLenDynamic(mat, 'b'));
+		System.out.println(getLen(mat, 'f'));
+		System.out.println(getLenDynamic(mat, 'f'));
+		System.out.println(getLenDynamic(mat, 'd'));
+	}
+
+	private static int getLenDynamic(char[][] mat, char ch) {
+		int ans = 0;
+		int m = mat.length;
+		int n = mat[0].length;
+		int i, j;
+		int[][] dp = new int[m][n];
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < n; j++)
+				dp[i][j] = -1;
+		}
+		for (i = 0; i < m; i++) {
+			for (j = 0; j < n; j++) {
+				if (mat[i][j] == ch) {
+					ans = Math.max(ans,
+							1 + getLenDynamicUtil(mat, i, j, m, n, ch, dp));
+				}
+			}
+		}
+		return ans;
+	}
+
+	private static int getLenDynamicUtil(char[][] mat, int x, int y, int m,
+			int n, char ch, int[][] dp) {
+		if (x < 0 || y < 0 || x >= m || y >= n)
+			return 0;
+		if (dp[x][y] != -1)
+			return dp[x][y];
+		int[] dx = { -1, -1, -1, 0,  0, 1, 1, 1 };
+		int[] dy = { -1, 0, 1, -1,  1, -1, 0, 1 };
+		int i, j;
+		int ans = 0;
+		for (i = 0; i < 8; i++) {
+			// for(j=0;j<8;j++)
+			// {
+			int x1 = x + dx[i];
+			int y1 = y + dy[i];
+			if (x1 < 0 || y1 < 0 || x1 >= m || y1 >= n)
+				continue;
+			if (mat[x1][y1] - ch == 1) {
+				ans = Math.max(ans,
+						1 + getLenDynamicUtil(mat, x1, y1, m, n, mat[x1][y1], dp));
+			}
+
+			// }
+		}
+		dp[x][y] = ans;
+		return dp[x][y];
+	}
+
+	private static int getLen(char[][] mat, char ch) {
+
+		int i, j;
+		int ans = 0;
+		int m = mat.length;
+		int n = mat[0].length;
+		for (i = 0; i < m; i++) {
+			for (j = 0; j < n; j++) {
+				if (mat[i][j] == ch) {
+					// found a character
+					ans = Math.max(ans, getLenHelper(mat, ch, i, j, m, n));
+				}
+			}
+		}
+		return ans;
+	}
+
+	private static int getLenHelper(char[][] mat, char ch, int x1, int y1,
+			int m, int n) {
+		int[] dx = { -1, -1, -1, 0,  0, 1, 1, 1 };
+		int[] dy = { -1, 0, 1, -1,  1, -1, 0, 1 };
+		int ans = 1;
+		int i, j;
+		for (i = 0; i < 8; i++) {
+			int x = x1 + dx[i];
+			int y = y1 + dy[i];
+			if (x >= 0 && y >= 0 && x < m && y < n) {
+				if (mat[x][y] - ch == 1) {
+					ans = Math.max(ans,
+							1 + getLenHelper(mat, mat[x][y], x, y, m, n));
+				}
+			}
+		}
+		return ans;
+	}
+
+}
